d69a3451c229c34dadfa040adecd69df7cbe6b94
James Cook <jamescookjunior@gmail.com>
2026-01-21 18:38:51 -0300

    feat: removi botões na tela

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
 import { Scenario } from "./components/Scenario";
-import { ChatInterface } from "./components/ChatInterface";
 
 // NÃO chame useSpeech() aqui fora. O hook já está sendo usado dentro dos componentes filhos (Avatar/ChatInterface).
 
@@ -13,9 +12,7 @@ function App() {
       {/* Esconde os controles de debug na apresentação */}
       <Leva collapsed hidden /> 
       
-      {/* Interface de Chat (opcional, pode remover se quiser tela limpa pro Totem) */}
-      <ChatInterface /> 
-      
+      {/* Interface de Chat (opcional, pode remover se quiser tela limpa na apresentação) */}      
       <Canvas shadows camera={{ position: [0, 0, 0], fov: 10 }}>
         <Scenario />
       </Canvas>
